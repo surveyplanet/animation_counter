88e5c077965e9c9b49cbe468b0455fcb030e90db
serferdinand2 <ognjen@surveyplanet.com>
2022-10-06 20:53:00 +0200

    fixed an issue where count would not show periods untill the end

--- index.js
+++ index.js
@@ -21,79 +21,70 @@
  *    localize: 'de-DE',
  * });
  */
-const animationCounter = (el, options = {}) =>
-{
-    if (Object.prototype.toString.call(el) === '[object String]')
-    {
-        el = document.querySelector(el);
-    }
+const animationCounter = (el, options = {}) => {
+	if (Object.prototype.toString.call(el) === '[object String]') {
+		el = document.querySelector(el);
+	}
 
-    options = {
-        ...
-        {
-            startVal: 0,
-            endVal: 100,
-            interval: 10,
-            step: 1,
-        },
-        ...options,
-    };
-    if (isNaN(options.startVal))
-    {
-        throw new Error('startVal must be a number');
-    }
-    if (isNaN(options.endVal))
-    {
-        throw new Error('endVal must be a number');
-    }
-    if (isNaN(options.interval))
-    {
-        throw new Error('interval must be a number');
-    }
-    if (isNaN(options.step))
-    {
-        throw new Error('step must be a number');
-    }
+	options = {
+		...{
+			startVal: 0,
+			endVal: 100,
+			interval: 10,
+			step: 1,
+		},
+		...options,
+	};
+	if (isNaN(options.startVal)) {
+		throw new Error('startVal must be a number');
+	}
+	if (isNaN(options.endVal)) {
+		throw new Error('endVal must be a number');
+	}
+	if (isNaN(options.interval)) {
+		throw new Error('interval must be a number');
+	}
+	if (isNaN(options.step)) {
+		throw new Error('step must be a number');
+	}
 
-    options.startVal = Number(options.startVal);
-    options.endVal = Number(options.endVal);
-    options.interval = Number(options.interval);
-    options.step = Number(options.step);
+	options.startVal = Number(options.startVal);
+	options.endVal = Number(options.endVal);
+	options.interval = Number(options.interval);
+	options.step = Number(options.step);
 
-    return new Promise((resolve, reject) =>
-    {
-        if (options.interval === 0)
-        {
-            el.textContent = options.endVal;
-            return resolve();
-        }
+	return new Promise((resolve, reject) => {
+		if (options.interval === 0) {
+			el.textContent = options.endVal;
+			return resolve();
+		}
 
-        let count = options.startVal;
+		let count = options.startVal;
 
-        const counter = setInterval(() =>
-        {
-            count += options.step;
-            el.textContent = count;
-            if (count >= options.endVal)
-            {
+		const counter = setInterval(() => {
+			count += options.step;
+			el.textContent =
+				options.localize && options.localize.length
+					? Intl.NumberFormat(options.localize, {
+							maximumSignificantDigits: 3,
+					  }).format(count)
+					: count;
+			if (count >= options.endVal) {
+				let endVal =
+					options.localize && options.localize.length
+						? Intl.NumberFormat(options.localize, {
+								maximumSignificantDigits: 3,
+						  }).format(options.endVal)
+						: options.endVal;
 
-                let endVal =
-                    options.localize && options.localize.length ?
-                    Intl.NumberFormat(options.localize,
-                    {
-                        maximumSignificantDigits: 3,
-                    }).format(options.endVal) :
-                    options.endVal;
+				el.textContent = endVal;
 
-                el.textContent = endVal;
+				clearInterval(counter);
 
-                clearInterval(counter);
-
-                return resolve();
-
-            }
-        });
-    });
+				return resolve();
+			}
+		});
+	});
 };
 
 export default animationCounter;
